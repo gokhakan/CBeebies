15296b432c89829c4e363783ee57636034e563bd
gokhakan <71521573+gokhakan@users.noreply.github.com>
2024-11-03 17:29:48 +0000

    update base

--- src/test/java/com/CBeebies/pages/Base.java
+++ src/test/java/com/CBeebies/pages/Base.java
@@ -11,4 +11,18 @@ public class Base {
         supertato.click();
 
     }
+
+    @FindBy(xpath = "//button[@aria-label='Right swipe - select to move to the right']//div[@class='src__Click-sc-1e9l4qf-2 fQFOfF']")
+    public WebElement rightButton;
+
+    @FindBy(xpath = "//button[@aria-label='Left swipe - select to move to the left']//div[@class='src__Click-sc-1e9l4qf-2 fQFOfF']")
+    public WebElement leftButton;
+
+    public void clickRightButton() {
+        rightButton.click();
+    }
+
+    public void clickLeftButton() {
+        leftButton.click();
+    }
 }
